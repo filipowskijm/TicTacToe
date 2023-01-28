926053defcacd5b4122620d75ad5f1309d698640
Jake Filipowski <filipowskijm@gmail.com>
2023-01-28 13:13:55 -0500

    fixed reset button function, added other box click functions

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+// reset button function
+function reset() {
+    location.reload();
+    document.getElementById('square1').value = '';
+    document.getElementById('square2').value = '';
+    document.getElementById('square3').value = '';
+    document.getElementById('square4').value = '';
+    document.getElementById('square5').value = '';
+    document.getElementById('square6').value = '';
+    document.getElementById('square7').value = '';
+    document.getElementById('square8').value = '';
+    document.getElementById('square9').value = '';
+}
+
+// click boxes and return X or O based on turn
 turn = 1
-// let square = document.getElementById('square1, square2, square3, square4, square5, square6, square7, square8, square9')
 function clickSquare1() {
     if (turn == 1) {
         document.getElementById('square1').value = "X";
@@ -14,3 +28,119 @@ function clickSquare1() {
         console.log('clicked O')
     }
 }
+function clickSquare2() {
+    if (turn == 1) {
+        document.getElementById('square2').value = "X";
+        document.getElementById('square2').disabled = true;
+        turn = 0;
+        console.log('clicked X')
+    }
+    else {
+        document.getElementById('square2').value = "O";
+        document.getElementById('square2').disabled = true;
+        turn = 1;
+        console.log('clicked O')
+    }
+}
+function clickSquare3() {
+    if (turn == 1) {
+        document.getElementById('square3').value = "X";
+        document.getElementById('square3').disabled = true;
+        turn = 0;
+        console.log('clicked X')
+    }
+    else {
+        document.getElementById('square3').value = "O";
+        document.getElementById('square3').disabled = true;
+        turn = 1;
+        console.log('clicked O')
+    }
+}
+function clickSquare4() {
+    if (turn == 1) {
+        document.getElementById('square4').value = "X";
+        document.getElementById('square4').disabled = true;
+        turn = 0;
+        console.log('clicked X')
+    }
+    else {
+        document.getElementById('square4').value = "O";
+        document.getElementById('square4').disabled = true;
+        turn = 1;
+        console.log('clicked O')
+    }
+}
+function clickSquare5() {
+    if (turn == 1) {
+        document.getElementById('square5').value = "X";
+        document.getElementById('square5').disabled = true;
+        turn = 0;
+        console.log('clicked X')
+    }
+    else {
+        document.getElementById('square5').value = "O";
+        document.getElementById('square5').disabled = true;
+        turn = 1;
+        console.log('clicked O')
+    }
+}
+function clickSquare6() {
+    if (turn == 1) {
+        document.getElementById('square6').value = "X";
+        document.getElementById('square6').disabled = true;
+        turn = 0;
+        console.log('clicked X')
+    }
+    else {
+        document.getElementById('square6').value = "O";
+        document.getElementById('square6').disabled = true;
+        turn = 1;
+        console.log('clicked O')
+    }
+}
+function clickSquare7() {
+    if (turn == 1) {
+        document.getElementById('square7').value = "X";
+        document.getElementById('square7').disabled = true;
+        turn = 0;
+        console.log('clicked X')
+    }
+    else {
+        document.getElementById('square7').value = "O";
+        document.getElementById('square7').disabled = true;
+        turn = 1;
+        console.log('clicked O')
+    }
+}
+function clickSquare8() {
+    if (turn == 1) {
+        document.getElementById('square8').value = "X";
+        document.getElementById('square8').disabled = true;
+        turn = 0;
+        console.log('clicked X')
+    }
+    else {
+        document.getElementById('square8').value = "O";
+        document.getElementById('square8').disabled = true;
+        turn = 1;
+        console.log('clicked O')
+    }
+}
+function clickSquare9() {
+    if (turn == 1) {
+        document.getElementById('square9').value = "X";
+        document.getElementById('square9').disabled = true;
+        turn = 0;
+        console.log('clicked X')
+    }
+    else {
+        document.getElementById('square9').value = "O";
+        document.getElementById('square9').disabled = true;
+        turn = 1;
+        console.log('clicked O')
+    }
+}
+
+
+
+
